43476ef5ec576eb522428f9de58b02698f152bb6
Сергей <zaswed76@gmail.com>
2020-03-03 21:09:55 +0200

    add post edit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 from django.utils import timezone
 # Create your views here.
 
@@ -10,3 +11,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
